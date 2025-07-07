48d78af0c09795bd30cf7cb112b032fcc1eaa04d
anoncpa <anonjcpa@gmail.com>
2025-07-07 21:26:01 +0900

    tsupのビルドOK

---
+++ packages/parser/tsup.config.ts
@@ -0,0 +1,13 @@
+// packages/parser/tsup.config.ts
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: {
+    compilerOptions: {
+      composite: false,
+    },
+  },
+  clean: true,
+});
